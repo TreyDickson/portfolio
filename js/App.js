14dd91652eef6ce9bc1d7b9d33de6b02921a644a
Trey Dickson <trey_dickson@wgresorts.com>
2014-05-26 13:40:30 -0400

    styling first pass

--- js/App.js
+++ js/App.js
@@ -3,6 +3,8 @@
 var App = angular.module('Portfolio', ['ngSanitize']);
 
 App.controller("MainController", ['$scope', '$http', function($scope, $http) {
+	$scope.nojs = false;
+
 	$scope.loadJson = function() {
 		$http.get("content/content.json").success(function(content) {
 			$scope.content = content;
